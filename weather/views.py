0b7e5eeb3cb891d1978ce6f795566c0344d0ed99
Darius <headshrinker89@gmail.com>
2019-01-23 16:34:43 +0000

    Create index page for weather of one city

--- weather/views.py
+++ weather/views.py
@@ -1,5 +1,23 @@
+import requests
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'weather/weather.html')
+    url = 'http://samples.openweathermap.org/data/2.5/find?q={}&units=metric&appid=b6907d289e10d714a6e88b30761fae22'
+
+    city = 'London'
+    r = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city': city,
+        'temperature': r['list'][0]['main']['temp'],
+        'description': r['list'][0]['weather'][0]['description'],
+        'icon': r['list'][0]['weather'][0]['icon']
+    }
+
+    print(city_weather)
+
+    context = {'city_weather': city_weather}
+
+    return render(request, 'weather/weather.html', context)
+
