871addc00d6ba247e9c8131f19db4c81769ab7a4
Darius <headshrinker89@gmail.com>
2019-01-29 17:12:27 +0000

    Get live weather for multiple cities

--- weather/views.py
+++ weather/views.py
@@ -1,23 +1,28 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 
 def index(request):
-    url = 'http://samples.openweathermap.org/data/2.5/find?q={}&units=metric&appid=b6907d289e10d714a6e88b30761fae22'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=08daed9c87b5bb0b625b74e777a6762c'
 
-    city = 'London'
-    r = requests.get(url.format(city)).json()
+    weather_list = []
+    cities = City.objects.all()
 
-    city_weather = {
-        'city': city,
-        'temperature': r['list'][0]['main']['temp'],
-        'description': r['list'][0]['weather'][0]['description'],
-        'icon': r['list'][0]['weather'][0]['icon']
-    }
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+        print(city_weather)
+        weather_list.append(city_weather)
 
-    print(city_weather)
+    print(weather_list)
 
-    context = {'city_weather': city_weather}
+    context = {'weather_list': weather_list}
 
     return render(request, 'weather/weather.html', context)
 
