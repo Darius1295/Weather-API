9aa6e026aedfba6d98b96440b44c8b8758976c60
Darius Corrigall <Darius@MacBook-Pro.JOOWIN>
2022-12-23 22:13:00 +0000

    Deploying to Railway

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("WEATHER_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG_VALUE") == "True"
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', 'django-weather-api.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 COUNTRIES_FIRST = [
     'GB', 'US'
@@ -170,5 +170,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-django_heroku.settings(locals())
